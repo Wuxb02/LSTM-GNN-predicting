3fadf29dbaf6a5dd3a62fbd76924d7a137d8f5ae
wxb <wuxb55@mail2.sysu.edu.cn>
2026-01-19 17:39:26 +0800

    内容更新

--- myGNN/graph/__init__.py
+++ myGNN/graph/__init__.py
@@ -3,11 +3,9 @@ myGNN图结构模块
 
 包含所有图构建和管理功能:
 - distance_graph.py: 基于距离的图构建（K近邻、逆距离权重、空间相似性）
-- dynamic_graph.py: 动态图学习模块
 """
 
 from .distance_graph import (
-    Graph_full,
     Graph_knn,
     Graph_inv_dis,
     Graph_spatial_similarity,
@@ -19,7 +17,6 @@ from .distance_graph import (
 
 __all__ = [
     # 静态图类
-    'Graph_full',
     'Graph_knn',
     'Graph_inv_dis',
     'Graph_spatial_similarity',
